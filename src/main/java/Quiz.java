68592680777576cf649e93d0ff2a5a5589c3b7ae
Awan53 <mubarokg@gmail.com>
2024-12-14 15:20:10 +0700

    Tambahan_OOP

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,120 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+/*
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Masukkan kata yang ingin di proses");
+        String strText = scn.next();
+        String poli ="";
+        StringBuilder stringBuilder = new StringBuilder();
+        String strTest = "";
+
+        stringBuilder.setLength(0);
+        for (int i=strText.length(); i>1; i++){
+            System.out.println(strText.substring(i-1, i)+"i =" +i);
+            strTest = strTest.substring(i-1, i);
+            poli = stringBuilder.append(strTest).toString();
+        }
+
+        stringBuilder.setLength(0);
+        if(strText.equalsIgnoreCase(poli)){
+            System.out.println("Merupakan anagram");
+        }else
+        {
+            System.out.println("Bukan Anagram");
+        }
+        */
+//        String strThird = "welcome";
+//
+//        System.out.println(strThird.substring(4));
+
+//        int intLoop  = 0;
+//        boolean isGood = true;
+//        do {
+//            intLoop++;
+//
+//        } while (isGood);
+//        System.out.println(isGood);
+
+
+//        int intLoop = 0;
+//        for (int i = 0; i<10; i++){
+//            intLoop++;
+//            for (int j = 0; j<20; j++){
+//                if(j==10)
+//                {
+//                   break;
+//                }
+//                intLoop++;
+//            }
+//        }
+//        System.out.println(intLoop);
+
+
+       /* String strSambung = "8"+3;
+        System.out.println(strSambung);*/
+//        int intLoop = 0;
+//        boolean isGood = true;
+//        do{
+//            intLoop++;
+//                    continue;
+//        }while (!isGood);
+//        System.out.println(intLoop);
+
+//        if ((5<3) && (7 != 2))
+//        {
+//            System.out.println("Ok");
+//        } else if ((4 == 4)|| !(3>2)) {
+//            System.out.println("yes");
+//
+//        }else{
+//            System.out.println("No");
+//        }
+//        int intLoop = 0;
+//        for (int i= 0; i<5; i++ ){
+//            intLoop++;
+//            if (intLoop>5){
+//                break;
+//            }
+//        }
+//        System.out.println(intLoop);
+//    String a ="Kemarau";
+//
+//    String output = (a.equals("kemarau"))? "1":"2";
+//    System.out.println(output);
+//
+//        int Intloop = 0;
+//        for (int i = 0; i <5; i++){
+//            Intloop++;
+//            continue;
+//        }
+//        System.out.println(Intloop);
+
+
+//        String strThrid = "Welcome";
+//        System.out.println(strThrid.substring(6,7));
+//        Random rand = new Random();
+//        for (int i = 0; i<10; i++){
+//            System.out.println("Random Number "+i+"->"+rand.nextInt(70,100));
+//        }
+//
+//        int angka = 11;
+//
+//        if (angka % 2 == 0) {
+//            System.out.println("Angka "+angka+" Adalah bilangan ganjil");
+//
+//        }else {
+//            System.out.println("Angka "+angka+" Adalah bilangan genap");
+//        }
+//
+//        int intLoop = 0;
+//        for (int i =0; i<10; i++)
+//        { intLoop++; for (int j = 0; j<20;j++){ if (j==10){ break;} intLoop++;}
+//        }
+//        System.out.println(intLoop);
+    }
+
+}
